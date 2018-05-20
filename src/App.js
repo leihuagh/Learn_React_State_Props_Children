0418d7af89bb0c20a29a7bbda9171af1ed329856
leihua <leihua@masslottery.com>
2018-05-19 23:27:50 -0400

    user and users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import Users from './components/endpoints/users/Users'
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <Users />
       </div>
     )
   }
